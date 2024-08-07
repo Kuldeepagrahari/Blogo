e0cc4086ace098ffa544834234e8b9ba5950eaad
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-08-08 02:25:40 +0530

    frontend#

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
+import "./admin.css"
 const Admin = () => {
   return (
-    <div>
+    <div className='admin'>
  
-        
+        <div className="admin-left">
+
+        </div>
+        <div className="admin-right">
+           <h1>Contact Me</h1>
+           <div className="admin-contact">
+            <form action="">
+              <input type="text" placeholder='Your Name'/>
+              <input type="email" name="" id="" placeholder='Your Email'/>
+              <textarea name="" id="" cols="30" rows="10" placeholder='write your message'></textarea>
+              <button type='submit'>Send</button>
+            </form>
+             
+
+           </div>
+        </div>
         
       
     </div>
