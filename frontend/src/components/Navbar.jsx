3a855cc447d3d2f6db88d5802c90846669ee3ee4
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-08-08 15:58:22 +0530

    contact page is done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import "./compo.css"
-
+import logo from "../assets/Blogger.png"
 import Login from '../pages/Login'
 const Navbar = () => {
 
   var isLogin = true;
   return (
     <div className='nav'>
-      <span className='logo'><h1>Sam</h1><h1>BLOGS.</h1></span>
+      <span className='logo'><img src={logo} style={{height:"11vh", borderRadius:"10px",boxShadow:"5px 5px 10px black"}}alt="" /></span>
       <ul>
         <NavLink style={({ isActive }) => ({ borderBottom: isActive ? '3px solid violet' : '' , paddingBottom: isActive ? '0.5vh' : ""})} to="/"><li>Home</li></NavLink>
         <NavLink style={({ isActive }) => ({ borderBottom: isActive ? '3px solid violet' : '' , paddingBottom: isActive ? '0.5vh' : ""})} to="/myblogs"><li>Write Blogs</li></NavLink>
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import "./admin.css"
+import { Link } from 'react-router-dom'
+import { BsGithub } from "react-icons/bs";
+import { BsTwitterX } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
 const Admin = () => {
   return (
     <div className='admin'>
  
         <div className="admin-left">
+          <div className="admin-left-left">
+              <img src="" alt="me" />
+              <h1>Kuldeep Agrahari</h1>
+              <h4>Btech 2026 || CSE || MERN Stack Developer || PDPM IIITDM Jabalpur</h4>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam illo cumque molestiae numquam voluptas itaque tempora explicabo? Illo deserunt temporibus quibusdam, ducimus aperiam suscipit, obcaecati voluptas excepturi ipsum asperiores velit.</p>
+              
 
+          </div>
+          <div className="admin-left-right">
+           <div className="admin-left-right-top">
+             <h1>Follow Me</h1>
+             <Link><FaLinkedin/></Link>
+             <Link><BsTwitterX/></Link>
+             <Link><BsGithub/></Link>
+
+             <div className="my-blog-det">
+              
+             </div>
+           </div>
+           <div className="admin-left-right-bottom">
+
+           </div>
+
+
+          </div>
         </div>
         <div className="admin-right">
            <h1>Contact Me</h1>
